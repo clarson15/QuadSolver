e766db055d9fc5c42885e69b2d2b3cde0ceb1dd5
Charlie Larson <charles.w.larson@wmich.edu>
2018-03-12 00:37:22 -0400

    add validate.c basic functionality

--- src/validate/test.c
+++ src/validate/test.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main(int argc, char* argv[]){
+extern int validate(char*, float*, float*, float*);
 
+int main(int argc, char* argv[]){
+	float a, b, c;
+	char str[80];
+	sprintf(str, "01.2352 218.320 4238.302");
+	validate(str, &a, &b, &c);
+	printf("%s: %f %f %f\n", str, a, b, c);
+	sprintf(str, "102.202 -2320.002 10283.20");
+	validate(str, &a, &b, &c);
+	printf("%s: %f %f %f\n", str, a, b, c);
 	return 0;
 }
--- src/validate/validate.c
+++ src/validate/validate.c
@@ -1,8 +1,9 @@
 //dummy file to test makefiles
 #include <stdio.h>
 
-int validate(){
-	int a = 2;
-	a++;
-	return 0;
+int validate(char* str, float* a, float *b, float *c){
+	int err = 1;
+	sscanf(str, "%f %f %f", a, b, c);
+	//todo: more validation on floats before returning an error code
+	return err;
 }
