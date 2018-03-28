8e43f9a345f8afe145cffae5bb248b0147e93ddd
Alex Dekau <alex.potatofarms@gmail.com>
2018-03-27 21:08:46 -0400

    Create cunit basic test framework

--- spikes/test_cunit/test.c
+++ spikes/test_cunit/test.c
@@ -1,12 +1,41 @@
-#include "../cunit/cunit.h"
+#include <CUnit/Basic.h>
+#include <stdio.h>
 
+int init_suite(void) { return 0; }
+int clean_suite(void) { return 0; }
 int max(int, int);
+void max_test(void);
 
 int main (void) {
-	cunit_init();
-	assert_eq("max1", max(0,1), 2);
-	assert_eq("max2", max(55,2), 55);
-	assert_eq("max3", max(100,100), 100);
+	// Initialize CUnit
+	CU_pSuite pSuite = NULL;
+	if (CUE_SUCCESS != CU_initialize_registry() ) {
+		return CU_get_error();
+	}
+
+	// Create a test suite
+	pSuite = CU_add_suite("max_test_suite", init_suite, clean_suite);
+	if (pSuite == NULL) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	// Add the tests.
+	if (CU_add_test(pSuite, "max_test", max_test) == NULL) {
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_run_tests();
+	puts("");
+	CU_basic_show_failures(CU_get_failure_list());
+	puts("");
+}
+
+void max_test (void) {
+	CU_ASSERT(max(1,2) == 2);
+	CU_ASSERT(max(4,3) == 4);
 }
 
 int max(int i1, int i2) {
